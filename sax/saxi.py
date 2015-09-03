5b4cf6d76b828339967f6bdd4b9f727c04f32be7
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-03 10:57:02 +0200

    Failed to solve tok with `declarative` rules

--- sax/saxi.py
+++ sax/saxi.py
@@ -24,9 +24,14 @@ def like(stream, string):
     return b
 
 
+def eos(stream):
+    return peek(stream) == ''
+
+
 def tok(stream):
 
-    while peek(stream) != '':
+    while not eos(stream):
+        print('[debug]', stream.tell(), eos(stream))
         rules = OrderedDict([
             ('<!--', 'comment'),    # COMMENT TAG
             ('<!', 'doctype'),      # DOCTYPE TAG
@@ -42,35 +47,82 @@ def tok(stream):
                     yield tag(stream, kind)
                     break
             else:
-                if not peek(stream) == '':
-                    yield text(stream, 'text')
+                if not eos(stream):
+                    yield text(stream)
+                    # stream.read(1)  # Eww T_T
 
 
-def text(stream, kind):
+# def text(stream, kind):
+#     acc = ''
+#     tec = stream.read(1)
+#     while tec != '<' and tec != '':
+#         acc += tec
+#         tec = stream.read(1)
+
+#     if peek(stream) != '':  # post-rewind
+#         stream.seek(stream.tell() - 1)  # must rewind before '<'
+#         # only if not at the end.
+#     return kind, acc
+
+# TODO abstract text and tag, see README.org  Imperative code : factorization
+
+
+# def tag(stream, kind):
+#     acc = ''
+#     tac = stream.read(1)
+#     while tac != '>' and tac != '':
+#         acc += tac
+#         tac = stream.read(1)
+
+#     if tac == '':                        # PREMATURE EOF
+#         return 'error', acc
+#     else:
+#         acc = acc + '>'  # post-append
+#         # hold on, self closing ?
+#         return ('selfclosing', acc) if acc[-1] == '/' else (kind, acc)
+
+
+def take(pred, stream):
     acc = ''
-    tec = stream.read(1)
-    while tec != '<' and tec != '':
-        acc += tec
-        tec = stream.read(1)
-
-    if peek(stream) != '':
-        stream.seek(stream.tell() - 1)  # must rewind before '<'
-        # only if not at the end.
-    return kind, acc
-
-
-def tag(stream, kind):
-    acc = ''
-    tac = stream.read(1)
-    while tac != '>' and tac != '':
-        acc += tac
-        tac = stream.read(1)
-
-    if tac == '':                        # PREMATURE EOF
-        return 'error', acc
-    else:
-        # hold on, self closing ?
-        if acc[-1] == '/':
-            return 'selfclosing', acc + '>'            # SELFCLOSING
+    while not eos(stream):
+        if pred(peek(stream)):
+            acc += stream.read(1)
         else:
-            return kind, acc + '>'
+            break
+    return eos(stream), acc
+
+
+def parse_while(s, c, inclusive):
+    prem, acc = take(lambda k: k != c, s)
+    if prem:
+        return prem, acc
+    else:
+        if inclusive:
+            s.read(1)  # ugly but necessary drop
+            return prem, acc + c
+        else:
+            return prem, acc
+
+
+def tag(s, k):
+    premature, a = parse_while(s, '>', True)
+    if premature:
+        return ('error', a)
+    else:
+        if a[-2] == '/':
+            return ('selfclosing', a)
+        else:
+            return (k, a)
+
+
+def rewind(s, n):
+    p = s.tell()
+    s.seek(p - n if p > n else p)
+
+
+def text(s):
+    premature, a = parse_while(s, '<', False)
+    if premature:
+        rewind(s, 1)
+    else:
+        return ('text', a)
