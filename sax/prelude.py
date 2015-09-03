80157a224fafb6997aa1b42c9ef85bd964ca156c
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-04 01:25:17 +0200

    Bug, signature mismatch in peek, fixed.

--- sax/prelude.py
+++ sax/prelude.py
@@ -18,18 +18,18 @@ def peek(stream, off=1):
     return c
 
 
-def peek(stream, forward=1, span=0):
+def peek(stream, off=1, span=0):
     '''
     >>> bs = io.StringIO('aBcDeF')
-    >>> peek(bs, forward=2)
+    >>> peek(bs, off=2)
     'B'
-    >>> peek(bs, forward=2)
+    >>> peek(bs, off=2)
     'B'
-    >>> peek(bs, forward=2, span=2)
+    >>> peek(bs, off=2, span=2)
     'BcD'  # BUG
     '''
     # print('[TODO] fix bug in peek, see docstring.')
     p = stream.tell()
-    c = stream.read(forward+span)[span-1:]  # python can be weird...
+    c = stream.read(off+span)[span-1:]  # python can be weird...
     stream.seek(p)
     return c
