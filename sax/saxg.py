cb3a6f0313cf7706b1605a6721835b201cdc6024
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-04 00:29:08 +0200

    Consistency fix in saxg: no more bytes, name reuse
    - Both tokenizers deal with strings now. TODO? Maybe later have an
    abstraction to deal with both case it cleanly.
    
    - saxg uses full names opening, closing, instruction instead of
      otag, etag, ... easier to read, consistent with saxi.
    
    - also removed useless variables, imports, and fixed formatting.

--- sax/saxg.py
+++ sax/saxg.py
@@ -2,48 +2,46 @@
 Generator based XML tokenizer (SAX like)
 '''
 
-import io
 
-xml = '/home/agumonkey/code/embauches/grammarize/tree.xml'
 
 def peek(stream, forward=1, span=0):
     '''
-    >>> bs = io.BytesIO(b'aBcDeF')
+    >>> bs = io.StringIO('aBcDeF')
     >>> peek(bs, forward=2)
-    b'B'
+    'B'
     >>> peek(bs, forward=2)
-    b'B'
+    'B'
     >>> peek(bs, forward=2, span=2)
-    b'BcD' # BUG
+    'BcD'  # BUG
     '''
     # print('[TODO] fix bug in peek, see docstring.')
     p = stream.tell()
-    c = stream.read(forward+span)[span-1:] # python can be weird...
+    c = stream.read(forward+span)[span-1:]  # python can be weird...
     stream.seek(p)
     return c
 
 class UnknownElement(Exception): pass
 
-def root(s):
+def tok(s):
     c1 = peek(s)
-    if c1 == b'<':
+    if c1 == '<':
         c2 = peek(s, 2)
-        if c2 == b'?':
-            yield from inst(s)
-        elif c2 == b'!':
+        if c2 == '?':
+            yield from instruction(s)
+        elif c2 == '!':
             c3 = peek(s, 3)
-            if c3 == b'-':
+            if c3 == '-':
                 yield from comment(s)
-            elif c3 == b'D':
+            elif c3 == 'D':
                 yield from doctype(s)
             else:
-                raise UknownElement(s)
-        elif c2 == b'/':
-            yield from etag(s)
+                raise UnknownElement(s)
+        elif c2 == '/':
+            yield from closing(s)
         else:
-            yield from otag(s)
-    elif c1 == b'':
-        raise StopIteration # EOF
+            yield from opening(s)
+    elif c1 == '':
+        raise StopIteration  # EOF
     else:
         yield from text(s)
         s.seek(s.tell() - 1)
@@ -51,74 +49,74 @@ def root(s):
 
 def comment(s):
     c = s.read(1)
-    a = b''
-    while c != b'>' and c != b'':
+    a = ''
+    while c != '>' and c != '':
         a += c
         c = s.read(1)
-    yield ('comment', a + b'>') if c != b'' else ('error', a)
-    yield from root(s)
+    yield ('comment', a + '>') if c != '' else ('error', a)
+    yield from tok(s)
 
 
 def doctype(s):
     c = s.read(1)
-    a = b''
-    while c != b'>' and c != b'':
+    a = ''
+    while c != '>' and c != '':
         a += c
         c = s.read(1)
-    yield ('doctype', a + b'>') if c != b'' else ('error', a)
-    yield from root(s)
+    yield ('doctype', a + '>') if c != '' else ('error', a)
+    yield from tok(s)
 
 
-def otag(s):
+def opening(s):
     '''
     WARNING inclusive limit '>' marks the end, MUST be added outside the loop
     '''
     c = s.read(1)
-    a = b''
+    a = ''
     selfclosing = False
-    while c != b'>' and c != b'':
+    while c != '>' and c != '':
         a += c
         c = s.read(1)
 
-    if c == b'>' and a[-1:] == b'/':
+    if c == '>' and a[-1:] == '/':
         selfclosing = True
 
-    yield ('otag', a + b'>') if c != b'' else ('error', a)
+    yield ('opening', a + '>') if c != '' else ('error', a)
     if selfclosing:
-        yield ('etag', a + b'>') # reuse the whole 'text' to allow checks
-    yield from root(s)
+        yield ('closing', a + '>')  # reuse the whole 'text' to allow checks
+    yield from tok(s)
 
-def etag(s):
+def closing(s):
     '''
     WARNING inclusive limit '>' marks the end, MUST be added outside the loop
     '''
     c = s.read(1)
-    a = b''
-    while c != b'>' and c != b'':
+    a = ''
+    while c != '>' and c != '':
         a += c
         c = s.read(1)
-    yield ('etag', a + b'>') if c != b'' else ('error', a)
-    yield from root(s)
+    yield ('closing', a + '>') if c != '' else ('error', a)
+    yield from tok(s)
 
-def inst(s):
+def instruction(s):
     '''
     WARNING inclusive limit '>' marks the end, MUST be added outside the loop
     '''
     c = s.read(1)
-    a = b''
-    while c != b'>' and c != b'':
+    a = ''
+    while c != '>' and c != '':
         a += c
         c = s.read(1)
-    yield ('inst', a + b'>') if c != b'' else ('error', a)
-    yield from root(s)
+    yield ('instruction', a + '>') if c != '' else ('error', a)
+    yield from tok(s)
 
 def text(s):
     c = s.read(1)
-    a = b''
-    while c != b'<' and c != b'':
+    a = ''
+    while c != '<' and c != '':
         a += c
         c = s.read(1)
-    yield ('text', a) # if c != b'' else ('error', a)
-    if c != b'':
-        s.seek(s.tell() - 1) # Finally... still ugly code
-    yield from root(s)
+    yield ('text', a)  # if c != '' else ('error', a)
+    if c != '':
+        s.seek(s.tell() - 1)  # Finally... still ugly code
+    yield from tok(s)
