1e271cf340951c2483054edd1c2ce0eb645c1432
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-04 12:32:31 +0200

    Handcrafted YAPF formatting for XML literal.
    ... why not.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -27,7 +27,66 @@ def test_1():
 def test_xml():
     s = open('./samples/dbus.xml')
     t = xml(tok(s))
-    e = Root(children=[Instruction(instruction='<?xml version="1.0"?>'), Text(text=' '), Comment(comment='<!--*-nxml-*-->'), Text(text='\n'), Doctype(doctype='<!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"\n        "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">'), Text(text='\n\n'), Comment(comment='<!--\n  This file is part of systemd.\n\n  systemd is free software; you can redistribute it and/or modify it\n  under the terms of the GNU Lesser General Public License as published by\n  the Free Software Foundation; either version 2.1 of the License, or\n  (at your option) any later version.\n-->'), Text(text='\n\n'), Tag(name='<busconfig>', attrs=[], children=[Text(text='\n\n        '), Tag(name='<policy user="root">', attrs=[], children=[Text(text='\n                '), Tag(name='<allow own="org.freedesktop.timedate1"/>', attrs=[], children=[]), Text(text='\n                '), Tag(name='<allow send_destination="org.freedesktop.timedate1"/>', attrs=[], children=[]), Text(text='\n                '), Tag(name='<allow receive_sender="org.freedesktop.timedate1"/>', attrs=[], children=[]), Text(text='\n        ')]), Text(text='\n\n        '), Tag(name='<policy context="default">', attrs=[], children=[Text(text='\n                '), Tag(name='<allow send_destination="org.freedesktop.timedate1"/>', attrs=[], children=[]), Text(text='\n                '), Tag(name='<allow receive_sender="org.freedesktop.timedate1"/>', attrs=[], children=[]), Text(text='\n        ')]), Text(text='\n\n')]), Text(text='\n')])
+
+    e = Root(children=[
+        Instruction(instruction='<?xml version="1.0"?>'), Text(text=' '),
+        Comment(comment='<!--*-nxml-*-->'), Text(text='\n'), Doctype(
+            doctype='<!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-BUS '
+            'Bus Configuration 1.0//EN"\n        "http://www.freedesktop.org/'
+            'standards/dbus/1.0/busconfig.dtd">'),
+        Text(text='\n\n'),
+        Comment(comment='<!--\n  This file is part of systemd.\n\n  systemd '
+                'is free software; you can redistribute it and/or modify '
+                'it\n  under the terms of the GNU Lesser General Public '
+                'License as published by\n  the Free Software Foundation; '
+                'either version 2.1 of the License, or\n  (at your option) '
+                'any later version.\n-->'),
+        Text(text='\n\n'),
+        Tag(name='<busconfig>',
+            attrs=[],
+            children=[
+                Text(text='\n\n        '),
+                Tag(
+                    name='<policy user="root">',
+                    attrs=[],
+                    children=[Text(text='\n                '),
+                              Tag(
+                                  name='<allow own="org.freedesktop.'
+                                  'timedate1"/>',
+                                  attrs=[],
+                                  children=[]),
+                              Text(text='\n                '),
+                              Tag(
+                                  name='<allow send_destination="org.'
+                                  'freedesktop.timedate1"/>',
+                                  attrs=[],
+                                  children=[]),
+                              Text(text='\n                '),
+                              Tag(
+                                  name='<allow receive_sender="org.'
+                                  'freedesktop.timedate1"/>',
+                                  attrs=[],
+                                  children=[]),
+                              Text(text='\n        ')]),
+                Text(text='\n\n        '),
+                Tag(
+                    name='<policy context="default">',
+                    attrs=[],
+                    children=[Text(text='\n                '),
+                              Tag(
+                                  name='<allow send_destination="'
+                                  'org.freedesktop.timedate1"/>',
+                                  attrs=[],
+                                  children=[]),
+                              Text(text='\n                '),
+                              Tag(
+                                  name='<allow receive_sender="org.'
+                                  'freedesktop.timedate1"/>',
+                                  attrs=[],
+                                  children=[]),
+                              Text(text='\n        ')]),
+                Text(text='\n\n')]),
+        Text(text='\n')])
     assert_equal(t, e)
 
 
