e56dacb1feba39f7772533081060e15bd110c48d
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-04 12:12:55 +0200

    loop tokenizer emits token begin, end stream pos

--- sax/tokenizer/loop.py
+++ sax/tokenizer/loop.py
@@ -7,12 +7,20 @@ from sax.prelude import peek
 from sax.tokenizer.interface import comment, doctype, opening, \
     closing, selfclosing, instruction, text, error
 
+lc = 0   # line count
+
 
 def tok(stream):
 
     while peek(stream) != '':
         c = peek(stream)
         k = None
+
+        # offside accounting
+        if c == '\n':
+            global lc
+            lc += 1
+
         if c == '<':                             # * TAG
             cc = peek(stream, off=2)
             if cc == '?':
@@ -31,25 +39,28 @@ def tok(stream):
                 k = opening                      # OPEN TAG
 
             # TAG work here
+            psb = stream.tell()
             acc = ''
             tac = stream.read(1)
             while tac != '>' and tac != '':
                 acc += tac
                 tac = stream.read(1)
+            pse = stream.tell()
 
             if tac == '':                        # PREMATURE EOF
                 k = error
-                yield k, acc
+                yield k, acc, (psb, pse)
             else:
                 # hold on, self closing ?
                 if acc[-1] == '/':
                     k = selfclosing              # SELFCLOSING
 
                 acc += '>'
-                yield k, acc
+                yield k, acc, (psb, pse)
 
         else:                                    # TEXT
             k = text
+            psb = stream.tell()
             acc = ''
             tec = stream.read(1)
             while tec != '<' and tec != '':
@@ -59,4 +70,5 @@ def tok(stream):
             if peek(stream) != '':
                 stream.seek(stream.tell() - 1)  # must rewind before '<'
                 # only if not at the end.
-            yield k, acc
+            pse = stream.tell()
+            yield k, acc, (psb, pse)
