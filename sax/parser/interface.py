1433e9acf4ba37fe3126fecdab1a946b9ca9961c
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-04 12:05:26 +0200

    added docstring to parser.interface

--- sax/parser/interface.py
+++ sax/parser/interface.py
@@ -6,3 +6,18 @@ Text = namedtuple('Text', 'text')
 Comment = namedtuple('Comment', 'comment')
 Doctype = namedtuple('Doctype', 'doctype')
 Tag = namedtuple('Tag', 'name attrs children')
+
+
+def xml(stream):
+    '''Stream -> XML'''
+    raise NotImplementedError("Interface stub only")
+
+
+def is_balanced(stream):
+    '''XML balance predicate.'''
+    '''
+    is_balanced <foo></foo> -> True
+    is_balanced <foo>       -> False
+    is_balanced <foo></bar> -> False
+    '''
+    raise NotImplementedError("Interface stub only")
