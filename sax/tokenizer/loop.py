784b9a1c5b222b5334d19883a3358cc68d0ba920
Johan PONIN <johan.ponin.pro@gmail.com>
2015-09-08 23:20:49 +0200

    VERY imperative way to compute line_no, line_off.
    Long and tedious fiddling with lc, lo `pointers`.
    After which I understood that I had to save them before
    parsing new elements -> pre_lc, pre_lo.
    Then _update_(...) will ... update lc, lo on each stream.read(>0).
    To become pre_lc, pre_lo on next iteration (aka next element).
    
    WARNING: Using your text editor lines and columns to check may mislead
    you !!! Emacs counts TAB as 8 spaces, while `tok` counts it as 1 char.
    So I had a +-7 offset ... Fortunately I wasn't fooled for too long.
    
    Anyway, I should have commited the intermediate steps, that would have
    been very worthy educationally speaking.
    
    ps: There surely is a better, more solid way to manage this kind of
    lateral pre/post accounting. TODO

--- sax/tokenizer/loop.py
+++ sax/tokenizer/loop.py
@@ -7,19 +7,21 @@ from sax.prelude import peek
 from sax.tokenizer.interface import comment, doctype, opening, \
     closing, selfclosing, instruction, text, error
 
-lc = 0   # line count
+
+lc = 1  # line count
+lo = 0  # line offset
 
 
 def tok(stream):
+    global lo
+    global lc
 
     while peek(stream) != '':
+
         c = peek(stream)
         k = None
-
-        # offside accounting
-        if c == '\n':
-            global lc
-            lc += 1
+        pre_lc = lc
+        pre_lo = lo
 
         if c == '<':                             # * TAG
             cc = peek(stream, off=2)
@@ -42,33 +44,49 @@ def tok(stream):
             psb = stream.tell()
             acc = ''
             tac = stream.read(1)
+            _update_(tac)
+
             while tac != '>' and tac != '':
                 acc += tac
                 tac = stream.read(1)
+                _update_(tac)
             pse = stream.tell()
 
             if tac == '':                        # PREMATURE EOF
                 k = error
-                yield k, acc, (psb, pse)
+                yield k, acc, (psb, pse, pre_lc, pre_lo)
             else:
                 # hold on, self closing ?
                 if acc[-1] == '/':
                     k = selfclosing              # SELFCLOSING
 
                 acc += '>'
-                yield k, acc, (psb, pse)
+                yield k, acc, (psb, pse, pre_lc, pre_lo)
 
         else:                                    # TEXT
             k = text
             psb = stream.tell()
             acc = ''
             tec = stream.read(1)
+            _update_(tec)
             while tec != '<' and tec != '':
                 acc += tec
                 tec = stream.read(1)
+                _update_(tec)
 
             if peek(stream) != '':
                 stream.seek(stream.tell() - 1)  # must rewind before '<'
                 # only if not at the end.
+                lo -= 1  # lo is impacted. Maybe lc too ? T_T
+
             pse = stream.tell()
-            yield k, acc, (psb, pse)
+            yield k, acc, (psb, pse, pre_lc, pre_lo)
+
+
+def _update_(c):
+    global lc, lo
+    if c == '\n':
+        lc += 1
+        lo = 0
+    else:
+        lo += 1
