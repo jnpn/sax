61b3bdb1179acfe4f75277a1028b7d454127b4f2
Johan PONIN <johan.ponin.pro@gmail.com>
2022-01-23 18:21:02 +0100

    prepare tests for sax.names.names

---
+++ tests/test_names.py
@@ -0,0 +1,12 @@
+'''test
+from sax.parser.core import pp, xml
+pp(xml(lt.tok(open('./samples/cv.meta.xml'))))
+
+from sax.parser.core import pp, xml
+pp(xml(lt.tok(open('./samples/lclo.xml'))))
+
+from sax.parser.core import pp, xml
+pp(xml(lt.tok(open('./samples/cv.settings.xml'))))
+
+pp(xml(lt.tok(open('./samples/cv.xml'))))
+'''
