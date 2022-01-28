74569bfdf829937bb231131fa935f4b124b608cf
Johan PONIN <johan.ponin.pro@gmail.com>
2022-01-28 21:05:56 +0100

    rewrite for readability

--- sax/parser/interface.py
+++ sax/parser/interface.py
@@ -12,17 +12,21 @@ Comment = namedtuple('Comment', 'comment')
 Doctype = namedtuple('Doctype', 'doctype')
 
 class Tag:
+    '''
+    Generic XML Node (tag, attrs, children...)
+    '''
 
     def __init__(self, spec:str, children=[]):
-        self.tag_r    = '</?\s*(?P<tag>[a-zA-Z0-9:_\-]+)(\s+(?P<attrs>.*))?/? ?>'
-        self.kv_r     = '(?P<key>[a-zA-Z0-9:\-]+)(="(?P<val>[^"]+)")?'     ### thanks https://regexr.com/6e6rh
+        self.tag_r    = '</?\s*(?P<tag>[a-zA-Z0-9:_\-]+)(\s+(?P<attr>.*))?/? ?>'
+        self.kv_r     = '(?P<key>[a-zA-Z0-9:\-]+)(="(?P<val>[^"]+)")?'
+        ###                 ^-- thanks https://regexr.com/6e6rh
         self.spec     = spec
-        self.children = children[:]
+        self.children= children[:]
         m = re.match(self.tag_r, self.spec, re.MULTILINE + re.DOTALL)
         if m:
             g = m.groupdict()
             self.name = name(g['tag'])
-            a = g['attrs'] or ''
+            a = g['attr'] or ''
             self.attrs = [(name(k),v) for k,_,v in re.findall(self.kv_r, a)]
         else:
             raise MalformedXML(spec, ' fails', self.tag_r, m)
@@ -40,7 +44,8 @@ class Tag:
             and self.children == other.children
 
     def __repr__(self):
-        return f'<{self.__class__.__name__} {self.name} {self.attrs} {self.children}>'
+        cn = self.__class__.__name__
+        return f'<{cn} {self.name} {self.attrs} {self.children}>'
 
 def xml(stream):
     '''Stream -> XML'''
